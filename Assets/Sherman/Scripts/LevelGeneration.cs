d227e77286f750e54d4b085469867ec406776d7d
Sherman Lok <43489501+sherman-l@users.noreply.github.com>
2019-08-08 23:48:34 -0700

    Adding my own test scene, placeholder assets, and simple script to spawn level parts based off of camera position (taken from tutorial)

---
+++ Assets/Sherman/Scripts/LevelGeneration.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    private const float CAMERA_DISTANCE_SPAWN_LEVEL_PART = 100f;
+    [SerializeField] private Transform level_start;
+    [SerializeField] private Transform level_1;
+    [SerializeField] private Transform mainCamera;
+
+    private Vector3 lastEndPosition;
+
+    private void Awake()
+    {
+        lastEndPosition = level_start.Find("EndPosition").position;
+        int startLevelParts = 4;
+        for (int i = 0; i < startLevelParts; i++)
+        {
+            SpawnLevelPart();
+        }
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(mainCamera.Find("CameraRightEnd").position, lastEndPosition) < CAMERA_DISTANCE_SPAWN_LEVEL_PART)
+            SpawnLevelPart();
+    }
+
+    private void SpawnLevelPart()
+    {
+        Transform lastLevelPartTransform = SpawnLevelPart(lastEndPosition);
+        lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
+    }
+    private Transform SpawnLevelPart(Vector3 spawnPosition)
+    {
+        Transform levelPartTransform = Instantiate(level_1, spawnPosition, Quaternion.identity);
+        return levelPartTransform;
+    }
+
+}
---
+++ Assets/Sherman/Scripts/MoveCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/** For testing only! **/
+public class MoveCamera : MonoBehaviour
+{
+    private float speed = 3.2f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+    }
+}
