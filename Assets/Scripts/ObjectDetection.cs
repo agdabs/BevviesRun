890f8b04ff4a1b6548d3f8ea1f46a64ae039cfef
Michael Goh <mikegoh1997@gmail.com>
2019-09-22 00:06:22 -0700

    Added a few things
    - Added a script that creates headers in the Hierarchy
    - Added a tag for collectible
    - A script that destroys an object if it is a collectible
    - Added WASD support to the player controls

---
+++ Assets/Scripts/ObjectDetection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDetection : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject obj = collision.gameObject;
+        if (obj.tag == "Collectible")
+        {
+            Debug.Log("Collected: " + obj.name);
+            Destroy(obj);
+        }
+    }
+}
